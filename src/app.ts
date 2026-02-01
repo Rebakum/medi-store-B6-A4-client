a79af6e24f78f592cb699b711a318f99329ef06e
Your NameRebakum <anamikaeasmin@hotmail.com>
2026-02-01 18:08:42 +0600

    change ap.ts and server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,50 +1,33 @@
-
 import express, { Application } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import router from "./routes";
 import { notFound } from "./middleware/notFound";
-import { ensureSuperAdmin } from "./seedAmin/ensureSuperAdmin";
-import orderRouter from "./modules/order/order.route";
 import globalErrorHandler from "./middleware/globalErrorHandler";
 import path from "path";
-
-
-
-
+import config from "./config";
 
 const app: Application = express();
 
-// CORS with credentials
+
 app.use(
   cors({
-    origin: "http://localhost:3000", 
+    origin:config.frontend_url || "http://localhost:3000",
     credentials: true,
   })
 );
 
-//  cookie parser MUST
 app.use(cookieParser());
-
 app.use(express.json());
-
-ensureSuperAdmin();
-
-
-// MAIN ROUTE
-
-
 
 app.use("/api/v1", router);
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
-
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send("Server running successfully");
 });
 
 app.use(notFound);
 app.use(globalErrorHandler);
-
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 import app from "./app";
 import config from "./config";
 import { prisma } from "./lib/prisma";
+import { ensureSuperAdmin } from "./seedAmin/ensureSuperAdmin";
+
 const port = config.port || 5000;
 
 async function main() {
   try {
     await prisma.$connect();
     console.log("Connected to the database successfully");
+
+    if (config.auto_seed_super_admin === "true") {
+      await ensureSuperAdmin();
+    }
+
     app.listen(port, () => {
-      console.log(`server is runing on http://localhost:${port}`);
+      console.log(`server is running on http://localhost:${port}`);
     });
   } catch (error) {
     console.error("An error occurred", error);
@@ -16,4 +23,5 @@ async function main() {
     process.exit(1);
   }
 }
+
 main();
